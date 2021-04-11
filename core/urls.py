8f122e497b512e14894e7ac434de6a044ee877b1
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-04-11 11:05:12 +0300

    refactor

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('portfolio/', views.portfolio_details, name='portfolio')
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def portfolio_details(request):
+    return render(request, 'portfolio-details.html')
