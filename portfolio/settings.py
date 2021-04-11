eaea4f4c37069144096a0014df8663a9fcd8963c
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-04-11 12:09:37 +0300

    final

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["will-resume.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
